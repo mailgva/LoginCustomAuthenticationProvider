740d1fb33991ecbdea94fa1389bf830336114e49
Vladimir Gorbatenko <mail.gva@gmail.com>
2021-01-17 23:22:34 +0200

    first commit

---
+++ src/test/java/com/gorbatenko/logincustomauthenticationprovider/LoginCustomAuthenticationProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorbatenko.logincustomauthenticationprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginCustomAuthenticationProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
